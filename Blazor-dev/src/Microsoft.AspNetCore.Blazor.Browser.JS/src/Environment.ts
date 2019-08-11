d9bebb838793b56be2cb89aec695021c93727581
Pankaj Patro <patro.pankaj@gmail.com>
2019-08-11 19:20:32 +0800

    newchanges

--- Blazor-dev/src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Environment.ts
+++
@@ -1,6 +0,0 @@
-﻿// Expose an export called 'platform' of the interface type 'Platform',
-// so that consumers can be agnostic about which implementation they use.
-// Basic alternative to having an actual DI container.
-import { Platform } from './Platform/Platform';
-import { monoPlatform } from './Platform/Mono/MonoPlatform';
-export const platform: Platform = monoPlatform;
